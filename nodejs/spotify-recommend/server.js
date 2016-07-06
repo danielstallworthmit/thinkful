d4dd81d317a6722058c984945054dcba060eef82
Daniel Stallworth <danielstallworthmit@gmail.com>
2016-07-05 22:43:50 -0500

    Node async spotify calls, socket.io chatroom functionality.

--- nodejs/spotify-recommend/server.js
+++ nodejs/spotify-recommend/server.js
@@ -1,6 +1,7 @@
 var unirest = require('unirest');
 var express = require('express');
 var events = require('events');
+var async = require('async');
 
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
@@ -29,12 +30,38 @@ app.get('/search/:name', function(req, res) {
 
     searchReq.on('end', function(item) {
         var artist = item.artists.items[0].id;
-	//app.get('/artists/:id/related-artists', function(req, res) {
    	var related = getFromApi('artists/'+ artist +'/related-artists');
 
         related.on('end', function(item) {
-		var rels = item.artists
-		res.json(rels)
+		var rels = item; //console.log(rels);
+		var completed = 0;
+		async.forEach(rels.artists, function(rel_artist) {
+			var top_tracks = getFromApi('artists/'+ rel_artist.id +'/top-tracks',{
+			    country: 'US'
+			}); //console.log(top_tracks);
+
+			top_tracks.on('end', function(item){
+			    rel_artist.tracks = item.tracks
+			    completed += 1; console.log(rel_artist.id);
+			    checkComplete();
+			});
+			top_tracks.on('error', function(code) {
+				res.sendStatus(code); console.log('error on tracks');
+			});
+
+			//res.json(rels)
+		});
+
+		var checkComplete = function(){
+			if (completed === rels.artists.length) { console.log(rels.artists.length);
+				res.json(rels);
+			}
+		};
+	
+	});
+
+	related.on('error', function(code) {
+		res.sendStatus(code);
 	});
     });
 
