f04af448bec319d0fcbcaa6b10046a7e06b19121
Daniel Stallworth <danielstallworthmit@gmail.com>
2016-07-06 22:47:34 -0500

    Worked on functionality for chatroom app and pictionary app using socket.io

--- nodejs/chatroom/public/main.js
+++ nodejs/chatroom/public/main.js
@@ -1,21 +1,63 @@
 $(document).ready(function() {
+    var username = '';
+    var connected = false;
     var socket = io();
-    var input = $('input');
+    var connectedinfo = $('#connectedinfo')
+    var userinput = $('#userfield');
+    var input = $('#messageinput');
     var messages = $('#messages');
 
+    function addParticipantsMessage (data) {
+	var usermessage = '';
+	if (data.numUsers === 1) {
+	    usermessage += "There is only 1 participant in this chat";
+	} else {
+	    usermessage += "There are " + data.numUsers + " participants in this chat";
+	}
+	connectedinfo.html(usermessage);
+    }
+    
     var addMessage = function(message) {
         messages.append('<div>' + message + '</div>');
     };
+
+    userinput.on('keydown', function(event) {
+        if (event.keyCode != 13) {
+            return;
+        }
+
+        username = userinput.val();
+	var usermessage = username + ' joined';
+        addMessage(usermessage);
+	socket.emit('message', usermessage);
+	socket.emit('add user', username);
+        //input.val('');
+    });
+
 
     input.on('keydown', function(event) {
         if (event.keyCode != 13) {
             return;
         }
+	if (!username) {return;}
 
-        var message = input.val();
+        var message = username + ' says: ' + input.val();
         addMessage(message);
 	socket.emit('message', message);
         input.val('');
     });
+
     socket.on('message', addMessage);
+
+    socket.on('login', function (data) {
+	connected = true;
+	// Display the welcome message
+	//var message = "Welcome to Socket.IO Chat – ";
+	addParticipantsMessage(data);
+      });
+	
+    socket.on('user joined', function (data) {
+	//log(data.username + ' joined');
+	addParticipantsMessage(data);
+    });
 });
--- nodejs/chatroom/server.js
+++ nodejs/chatroom/server.js
@@ -8,13 +8,46 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+var numUsers = 0;
+
 io.on('connection', function (socket) {
+    var addedUser = false;
     console.log('Client connected');
+
+    socket.on('add user', function (username) {
+	if (addedUser) return;
+
+	// we store the username in the socket session for this client
+	socket.username = username;
+	++numUsers;
+	addedUser = true;
+	socket.emit('login', {
+	   numUsers: numUsers
+	});
+	// echo globally (all clients) that a person has connected
+	//socket.broadcast.emit('user joined', {
+	    //username: socket.username,
+	    //numUsers: numUsers
+	//});
+    });
 
     socket.on('message', function(message) {
         console.log('Received message:', message);
         socket.broadcast.emit('message', message);
     });
+
+    socket.on('disconnect', function () {
+	if (addedUser) {
+	--numUsers;
+
+	// echo globally that this client has left
+	socket.broadcast.emit('user left', {
+	    username: socket.username,
+	    numUsers: numUsers
+	});
+	}
+    });
+
 });
 
 server.listen(3000);
