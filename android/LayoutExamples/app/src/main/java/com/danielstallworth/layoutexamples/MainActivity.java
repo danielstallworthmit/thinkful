989f94032c1ebb5ced6538fd74af9e6a86669714
Daniel Stallworth <danielstallworthmit@gmail.com>
2016-07-08 16:52:50 -0500

    Android notes app start

--- android/LayoutExamples/app/src/main/java/com/danielstallworth/layoutexamples/MainActivity.java
+++ android/LayoutExamples/app/src/main/java/com/danielstallworth/layoutexamples/MainActivity.java
@@ -2,8 +2,13 @@ package com.danielstallworth.layoutexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.find_text);
+        final EditText searchText = (EditText) findViewById(R.id.searchText);
+        Button searchButton = (Button) findViewById(R.id.searchButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), searchText.getText().toString(), Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.show();
+            }
+        });
         //WebView webview = (WebView) findViewById(R.id.web_view);
         //webview.setWebViewClient(new WebViewClient());
         //webview.loadUrl("http://www.thinkful.com");
