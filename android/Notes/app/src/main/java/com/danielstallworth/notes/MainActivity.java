f0afb947e16442c5b8ab871e4536977d0ac8fcb9
Daniel Stallworth <danielstallworthmit@gmail.com>
2016-07-08 19:00:08 -0500

    Android Notes app

--- android/Notes/app/src/main/java/com/danielstallworth/notes/MainActivity.java
+++ android/Notes/app/src/main/java/com/danielstallworth/notes/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -11,6 +13,9 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private NoteListItemAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new NoteListItemAdapter(this, mRecyclerView);
+        mRecyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ android/Notes/app/src/main/java/com/danielstallworth/notes/NoteListItem.java
@@ -0,0 +1,16 @@
+package com.danielstallworth.notes;
+
+/**
+ * Created by danie on 7/8/2016.
+ */
+public class NoteListItem {
+    private String text;
+
+    public NoteListItem(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+}
