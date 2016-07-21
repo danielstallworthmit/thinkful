5bd895c5371c6075d80c6ffff31cbf99ea6aa0aa
Daniel Stallworth <danielstallworthmit@gmail.com>
2016-07-21 06:24:17 -0500

    Updated Pictionary with handling for 1 drawer, taking turns after choosing correct word, and drawer disconnecting.

--- nodejs/pictionary/public/main.js
+++ nodejs/pictionary/public/main.js
@@ -2,7 +2,7 @@ var pictionary = function() {
     var socket = io();
     var canvas, context;
     var drawing = false;
-    var guessBox, guesses, myturn;
+    var guessBox, guesses;
     var draw = function(position) {
         context.beginPath();
         context.arc(position.x, position.y,
@@ -17,11 +17,11 @@ var pictionary = function() {
     canvas.on('mousedown', function(event){
         drawing = true;
         canvas.on('mousemove', function(event) {
-            if (drawing && myturn) {
+            if (drawing) {
                 var offset = canvas.offset();
                 var position = {x: event.pageX - offset.left,
                         y: event.pageY - offset.top};
-                draw(position);
+                //draw(position);
                 socket.emit('draw', position);
             }
         });
@@ -29,6 +29,18 @@ var pictionary = function() {
     canvas.on('mouseup', function(event){
         drawing = false;
     });
+
+    var clearCanv = function(){
+        context.clearRect(0, 0, canvas[0].width, canvas[0].height);
+    }
+
+    var playMessage = function(status) {
+        $('#playMessage').text(status);
+    };
+
+    var wordMessage = function(words) {
+        $('#wordMessage').text(words);
+    };
 
     var onKeyDown = function(event) {
         if (event.keyCode != 13) { // Enter
@@ -50,39 +62,11 @@ var pictionary = function() {
         guesses.text('Current Guess: ' + guess);
     };
 
-    var rightGuess = function(){
-        guesses.text('You are correct. Your turn to draw.');
-    };
-
     socket.on('message', addGuess);
     socket.on('draw', draw);
-    socket.on('clearCanvas', function() {
-        context.clearRect ( 0 , 0 , canvas.width() , canvas.height() );
-    });
-    socket.on('wordGuessed', function(msg) {
-        socket.emit('message', rightGuess);
-        if(myturn = true) {
-            readytodraw.prop('value', 'Ready to draw!');
-        }
-    });
-    socket.on('youCanDraw', function(msg) {
-        if(myturn) {
-            myturn = false;
-            //status.text('status: online | Click Ready to draw! button to start drawing');
-        }
-        //chatcontent.append('<p>Click <strong>Ready to draw!</strong> button to draw.</p>');
-        //chatScrollDown();
-    });
-    socket.on('youDraw', function(word) {
-        myturn = true;
-        //canvas.css('background-color', '#fff');
-        myword = word; console.log(word);
-        //status.text('status: online | Your word is: ' + myword[0] + ' (difficulty: ' + myword[1] + ')');
-        //readytodraw.prop('value', 'Pass (' + timeleft + ')');
-        
-        // turn on drawing timer
-        //drawingTimer = setInterval( timerTick, 1000 );
-    });
+    socket.on('playMessage', playMessage);
+    socket.on('wordMessage', wordMessage);
+    socket.on('clearCanvas', clearCanv);
 };
 
 $(document).ready(function() {
--- nodejs/pictionary/server.js
+++ nodejs/pictionary/server.js
@@ -8,9 +8,6 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
-var users = [], canvas = [];
-var dictionary, currentWord, currentPlayer, drawingTimer;
-
 var WORDS = [
     "word", "letter", "number", "person", "pen", "class", "people",
     "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
@@ -27,46 +24,76 @@ var WORDS = [
     "space"
 ];
 
+var randUsers = []
+var numUsers = 0;
+var playerTurn = 0;
+var randWord = WORDS[Math.floor(Math.random()*WORDS.length)]
+console.log(randWord);
+
 io.on('connection', function(socket){
-	socket.emit('youDraw');
+	var randUser = Math.floor(Math.random()*1000000);
+	socket.username = 'user' + randUser;
+	randUsers.push(socket.username);
+    ++numUsers;
+    addedUser = true;
+    if (playerTurn < 1 || numUsers === 1) { playerTurn = socket.username; }
+	
+    if (playerTurn === socket.username) {
+    	socket.emit('wordMessage', 'Current Word to draw is ' + randWord);
+    }
+    var checkPlayerNumber = function (){
+		if (numUsers === 1) {
+    		io.emit('playMessage', 'Not enough players! Wait for others to join.');
+    	}
+    	else {
+    		io.emit('playMessage', 'There are enough players! Play Pictionary.');
+    	}
+	}
+    checkPlayerNumber();
+    
+    console.log(socket.username);
+    console.log('turn: ' + playerTurn);
+    console.log('users: ' + numUsers);
+
 	socket.on('message', function(message){
-		//console.log('Current Guess: ' + message);
+		console.log('Current Guess: ' + message);
+		if (message === randWord){
+			socket.emit('playMessage', 'That is correct! Now your turn to draw.');
+			socket.broadcast.emit('playMessage', '');
+			randWord = WORDS[Math.floor(Math.random()*WORDS.length)]
+			socket.emit('wordMessage', 'Current Word to draw is ' + randWord);
+			socket.broadcast.emit('wordMessage', '');
+			playerTurn = socket.username;
+			io.emit('clearCanvas');
+		}
 		io.emit('message', message);
 	});
 
 	socket.on('draw', function(position){
-		if(currentPlayer == socket.id) {
-			socket.broadcast.emit('draw', position);
+		if (playerTurn === socket.username){
+			io.emit('draw', position);
 		}
 	});
 
-	socket.on('clearCanvas', function () {
-		if(currentPlayer == socket.id) {
-			//canvas.splice(0, canvas.length);
+	socket.on('disconnect', function(){
+		--numUsers;
+		randUsers.splice(randUsers.indexOf(socket.username), 1);
+		if (numUsers > 1){
+			if (playerTurn === socket.username){
+				//playerTurn = randUsers[0];
+				socket.broadcast.emit('playMessage', 'Drawer left. Enter word below to become new drawer.');
+				socket.broadcast.emit('wordMessage', 'Current Word to draw is ' + randWord);
+				socket.broadcast.emit('clearCanvas');
+				//io.emit('wordMessage', 'Current Word to draw is ' + randWord);
+			}
+		}
+		else {
+			checkPlayerNumber();
+			playerTurn = randUsers[0];
+			socket.broadcast.emit('wordMessage', 'Current Word to draw is ' + randWord);
 			io.emit('clearCanvas');
 		}
 	});
-
-	socket.on('readyToDraw', function () {
-		if (!currentPlayer) {
-			currentPlayer = socket.id;
-			//canvas.splice(0, canvas.length);
-			io.emit('clearCanvas');
-			
-			var randomWord = WORDS[Math.floor(Math.random() * WORDS.length)];
-			
-			//currentWord = word[0];
-			socket.emit('youDraw', randomWord);
-			//io.sockets.emit('firendDraw', { color: myColor, nick: myNick });			
-		} else if (currentPlayer == socket.id) {
-			turnFinished();
-		}
-	});
-
-	function turnFinished() {
-		currentPlayer = socket.id;
-		io.sockets.emit('youCanDraw');
-	}
 });
 
-server.listen(3000);
+server.listen(3030);
