8a207141d99a3df4e3e3043c37da0f08e9441cb6
Daniel Stallworth <danielstallworthmit@gmail.com>
2016-07-23 15:31:19 -0500

    prepareForSegue, NSUserDefaults to save app state after closing.

--- ios/RGB Sliders/RGB Sliders/ViewController.swift
+++ ios/RGB Sliders/RGB Sliders/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         colorSquare.layer.borderColor = UIColor.blackColor().CGColor
         colorSquare.layer.borderWidth = 1
         
+        let defaults = NSUserDefaults.standardUserDefaults()
+        redSlider.value = defaults.floatForKey("red")
+        greenSlider.value = defaults.floatForKey("green")
+        blueSlider.value = defaults.floatForKey("blue")
+        
         updateBackgroundColor()
     }
     
@@ -28,12 +33,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "openColor") {
+            let newViewController = segue.destinationViewController
+            newViewController.view.backgroundColor = colorSquare.backgroundColor
+        }
+    }
+    
     @IBAction func updateBackgroundColor() {
         let red = CGFloat(redSlider.value)
         let green = CGFloat(greenSlider.value)
         let blue = CGFloat(blueSlider.value)
         
         colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setFloat(redSlider.value, forKey: "red")
+        defaults.setFloat(greenSlider.value, forKey: "green")
+        defaults.setFloat(blueSlider.value, forKey: "blue")
+        defaults.synchronize()
     }
     
 }
