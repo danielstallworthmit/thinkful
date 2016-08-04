0a7bac43024fc2c3d6fd31de801afb69782a4464
Daniel Stallworth <danielstallworthmit@gmail.com>
2016-08-04 13:47:01 -0500

    event services adjustment.

--- nodejs/calendar/services/event.js
+++ nodejs/calendar/services/event.js
@@ -11,14 +11,16 @@ exports.yrAgg = (year) => Event.aggregate([
 										  ])
 
 exports.month = (year, month) => Event.find().and([
-												{'$or': [{'startYear': year}, {'endYear': year}]},
-												{'$or': [{'startMonth': month}, {'endMonth': month}]}
+													{'$or': [{'startYear': year}, {'endYear': year}]},
+													{'$or': [{'startMonth': month}, {'endMonth': month}]}
 												])
 
 exports.days = (year, month, daysStart, daysEnd) => Event.find().and([
-																{'$or': [{'startYear': year}, {'endYear': year}]},
-																{'$or': [{'startMonth': month}, {'endMonth': month}]},
-																{'$or': [{'startDay': {'$gte': daysStart, '$lte': daysEnd}}, {'endDay': {'$gte': daysStart, '$lte': daysEnd}}]}
+																	{'$or': [{'startYear': year}, {'endYear': year}]},
+																	{'$or': [{'startMonth': month}, {'endMonth': month}]},
+																	{'$or': [{'startDay': {'$gte': daysStart, '$lte': daysEnd}}, 
+																			{'endDay': {'$gte': daysStart, '$lte': daysEnd}}]
+																	}
 																])
 
 exports.save = (event) => Event.create(event)
