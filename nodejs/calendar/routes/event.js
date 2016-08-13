30d60328e82a670a29b3ff406d49e2bf3f66cdf6
Daniel Stallworth <danielstallworthmit@gmail.com>
2016-08-13 10:56:12 -0500

    changed event route

--- nodejs/calendar/routes/event.js
+++ nodejs/calendar/routes/event.js
@@ -5,54 +5,54 @@ var passport = require('passport');
 
 var auth = passport.authenticate('basic', {session: false})
 
-router.get('/events/:yr', auth, (req, res) => {
+router.get('/event/:yr', auth, (req, res) => {
 	Event.yrAgg(Number(req.params.yr), req.user._id)
-		.then((events) => {res.status(200).json(events)})
+		.then((event) => {res.status(200).json(event)})
 		.catch((err) => res.status(400).json(err))
 });
 
-router.get('/events/:yr/:mth', auth, (req, res) => {
+router.get('/event/:yr/:mth', auth, (req, res) => {
 	Event.month(Number(req.params.yr), Number(req.params.mth), req.user._id)
-		.exec().then((events) => res.status(200).json(events))
+		.exec().then((event) => res.status(200).json(event))
 		.catch((err) => res.status(400).json(err))
 });
 
-router.get('/events/:yr/:mth/:startday', auth, (req, res) => {
+router.get('/event/:yr/:mth/:startday', auth, (req, res) => {
 	// var days = req.params.days.split('-')
 	//var daysStart = days[0], daysEnd = days[1]
 	Event.days(Number(req.params.yr), Number(req.params.mth), Number(req.params.startday), Number(req.params.startday), req.user._id)
-		.exec().then((events) => res.status(200).json(events))
+		.exec().then((event) => res.status(200).json(event))
 		.catch((err) => res.status(400).json(err))
 });
 
-router.get('/events/:yr/:mth/:startday/:endday', auth, (req, res) => {
+router.get('/event/:yr/:mth/:startday/:endday', auth, (req, res) => {
 	// var days = req.params.days.split('-')
 	//var daysStart = days[0], daysEnd = days[1]
 	Event.days(Number(req.params.yr), Number(req.params.mth), Number(req.params.startday), Number(req.params.endday), req.user._id)
-		.exec().then((events) => res.status(200).json(events))
+		.exec().then((event) => res.status(200).json(event))
 		.catch((err) => res.status(400).json(err))
 });
 
 
-router.get('/events', auth, function(req, res, next) { //console.log(req.user._id)
+router.get('/event', auth, function(req, res, next) { //console.log(req.user._id)
 	Event.list(req.user._id).exec()
-		.then((events) => res.status(200).json(events))
+		.then((event) => res.status(200).json(event))
 		.catch((err) => res.status(400).json(err))
 });
 
-router.post('/events', auth, function(req, res) {
+router.post('/event', auth, function(req, res) {
 	Event.save(req.body, req.user._id)
 		.then((event) => res.status(201).json(event))
 		.catch((err) => res.status(400).json(err))
 });
 
-router.put('/events/:id', auth, function(req, res) {
+router.put('/event/:id', auth, function(req, res) {
 	Event.update(req.params.id, req.body).exec()
 		.then((event) => res.status(200).json(event))
 		.catch((err) => res.status(400).json(err))
 });
 
-router.delete('/events/:id', auth, function(req, res) {
+router.delete('/event/:id', auth, function(req, res) {
 	Event.del(req.params.id).exec()
 		.then((event) => res.status(200).json(event))
 		.catch((err) => res.status(400).json(err))
